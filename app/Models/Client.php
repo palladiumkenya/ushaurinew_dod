1e0aebd64b997b824e27359d0bcc8cc052956424
Ronald Odhiambo <ronaldodhiambo27@gmail.com>
2021-02-08 11:21:02 +0300

    listing clients data in pmtct

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+    public $table = 'tbl_client';
+    public $timestamps = false;
+    public $incrementing = false;
+    
+    protected $fillable = [
+        
+    ];
 }
