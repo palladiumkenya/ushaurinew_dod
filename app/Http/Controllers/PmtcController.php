1cc6fbdd05498bfdadeec98571a84cfaf9ceaea5
Ronald Odhiambo <ronaldodhiambo27@gmail.com>
2021-02-09 11:35:42 +0300

    pmtct mother appointment track

--- app/Http/Controllers/PmtcController.php
+++ app/Http/Controllers/PmtcController.php
@@ -13,40 +13,41 @@ class PmtcController extends Controller
     public function get_pmtct_clients_data()
     {
         $data = [];
-
-        $all_booked_pmtct_clients = Pmtct::whereNotNull('appointment_date');
-        $all_pmtct_clients = Pmtct::whereNotNull('client_id');
-        $all_booked_pmtct_clients_count = Pmtct::whereNotNull('appointment_date');
+         
         
-        $test_client = Client::select('id')->where('clinic_id', '=', 2);
-        $honored_appointment_clients = Appointments::select('client_id')->where('appointment_kept', '=', 'Yes')->whereIn('client_id', $test_client);
-        $unschedule_clients = Appointments::select('client_id')->where('visit_type', '=', 'Un-Scheduled');
-        $scheduled_clients = Appointments::select('client_id')->where('visit_type', '=', 'Scheduled');
-        $missed_clients = Appointments::select('client_id')->where('app_status', '=', 'Missed');
-        $defaulted_clients = Appointments::select('client_id')->where('app_status', '=', 'Defaulted');
-        $ltfu_clients = Appointments::select('client_id')->where('app_status', '=', 'LTFU');
-        $deceased_clients = Client::select('id')->where('status', '=', 'Deceased');
+         // mother pmtct
+        $all_pmtct_clients = Pmtct::select('client_id')->whereNotNull('client_id');
+ 
+        //mother appointment
+        $honored_appointment_clients = Appointments::select('client_id')->where('appointment_kept', '=', 'Yes')->whereIn('client_id', $all_pmtct_clients);
+        $unschedule_appointment_clients = Appointments::select('client_id')->where('visit_type', '=', 'Un-Scheduled')->whereIn('client_id', $all_pmtct_clients);
+        $scheduled_appointment_clients = Appointments::select('client_id')->where('visit_type', '=', 'Scheduled')->whereIn('client_id', $all_pmtct_clients);
+        $missed_appointment_clients = Appointments::select('client_id')->where('app_status', '=', 'Missed')->whereIn('client_id', $all_pmtct_clients);
+        $defaulted_appointment_clients = Appointments::select('client_id')->where('app_status', '=', 'Defaulted')->whereIn('client_id', $all_pmtct_clients);
+        $ltfu_appointment_clients = Appointments::select('client_id')->where('app_status', '=', 'LTFU')->whereIn('client_id', $all_pmtct_clients);
+        $booked_appointment_clients = Appointments::select('client_id')->where('app_status', '=', 'Booked')->whereIn('client_id', $all_pmtct_clients);
 
-        $all_honored_appointment_clients = Pmtct::select('client_id')->whereIn('client_id', $honored_appointment_clients);
-        $all_unschedule_clients = Pmtct::select('client_id')->whereIn('client_id', $unschedule_clients);
-        $all_scheduled_clients = Pmtct::select('client_id')->whereIn('client_id', $scheduled_clients);
-        $all_missed_clients = Pmtct::select('client_id')->whereIn('client_id', $missed_clients);
-        $all_defaulted_clients = Pmtct::select('client_id')->whereIn('client_id', $defaulted_clients);
-        $all_ltfu_clients = Pmtct::select('client_id')->whereIn('client_id', $ltfu_clients);
-        $all_deceased_clients = Pmtct::select('client_id')->whereIn('client_id', $deceased_clients);
-
-
-        $data['all_booked_pmtct_clients_count'] = $all_booked_pmtct_clients_count->count();
-        $data['all_pmtct_clients'] = $all_pmtct_clients->count();
+        // mother as client
+        $all_honored_appointment_clients = Client::select('id', 'clinic_number')->whereIn('id', $honored_appointment_clients);
+        $all_unschedule_appointment_clients = Client::select('id', 'clinic_number')->whereIn('id', $unschedule_appointment_clients);
+        $all_schedule_appointment_clients = Client::select('id', 'clinic_number')->whereIn('id', $scheduled_appointment_clients);
+        $all_missed_appointment_clients = Client::select('id', 'clinic_number')->whereIn('id', $missed_appointment_clients);
+        $all_defaulted_appointment_clients = Client::select('id', 'clinic_number')->whereIn('id', $defaulted_appointment_clients);
+        $all_deceased_pmtct_clients = Client::select('id')->where('status', '=', 'Deceased')->whereIn('id', $all_pmtct_clients);
+        $all_ltfu_pmtct_clients = Client::select('id', 'clinic_number')->whereIn('id', $ltfu_appointment_clients);
+        $all_booked_pmtct_clients = Client::select('id', 'clinic_number')->whereIn('id', $booked_appointment_clients);
+        
+        
+        // mother data
         $data['all_honored_appointment_clients'] = $all_honored_appointment_clients->count();
+        $data['all_unschedule_appointment_clients'] = $all_unschedule_appointment_clients->count();
+        $data['all_schedule_appointment_clients'] = $all_schedule_appointment_clients->count();
+        $data['all_missed_appointment_clients'] = $all_missed_appointment_clients->count();
+        $data['all_defaulted_appointment_clients'] = $all_defaulted_appointment_clients->count();
+        $data['all_deceased_pmtct_clients'] = $all_deceased_pmtct_clients->count();
+        $data['all_ltfu_pmtct_clients'] = $all_ltfu_pmtct_clients->count();
         $data['all_booked_pmtct_clients'] = $all_booked_pmtct_clients->count();
-        $data['all_unschedule_clients'] = $all_unschedule_clients->count();
-        $data['all_scheduled_clients'] = $all_scheduled_clients->count();
-        $data['all_missed_clients'] = $all_missed_clients->count();
-        $data['all_defaulted_clients'] = $all_defaulted_clients->count();
-        $data['all_ltfu_clients'] = $all_ltfu_clients->count();
-        $data['all_deceased_clients'] = $all_deceased_clients->count();
-
+    
         return $data;
 
     }
